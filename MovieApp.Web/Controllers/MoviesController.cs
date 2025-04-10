cc218151b724684d4a0960d455fb779af2920e38
Hamza Gunes <your-email@example.com>
2025-04-10 18:14:15 +0300

    wwwroot, movie list and navbar add

--- MovieApp.Web/Controllers/MoviesController.cs
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MovieApp.Web.Models;
 
 namespace MovieApp.Web.Controllers
 {
@@ -12,7 +14,13 @@ namespace MovieApp.Web.Controllers
 
         public IActionResult List()
         {
-            return View("Movies");
+            var filmListesi = new List<Movie>()
+            {
+                new Movie {Title="film 1",Description="açıklama 1",Director="yonetmen 1",Players=new string[]{"player 1","player 2","player 3"},ImageUrl="fightclub.jpg"},
+                new Movie {Title="film 2",Description="açıklama 2",Director="yonetmen 2",Players=new string[]{"player 1","player 2","player 3"},ImageUrl ="godfather.jpg"},
+                new Movie {Title="film 3",Description="açıklama 3",Director="yonetmen 3",Players=new string[]{"player 1","player 2","player 3"},ImageUrl ="interstaller.jpg"}
+            };  
+            return View("Movies",filmListesi);
         }
 
         public IActionResult Details()
--- MovieApp.Web/Models/Movie.cs
+++ MovieApp.Web/Models/Movie.cs
@@ -6,6 +6,7 @@
         public string Description { get; set; }
         public string Director { get; set; }
         public string[] Players { get; set; }
+        public string ImageUrl { get; set; }
 
     }
 }
